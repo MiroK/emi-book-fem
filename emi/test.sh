a1eb2240334a7963cc51373fdba6aa0051e4aefd
mirok <miroslav.kuchta@gmail.com>
2020-04-27 14:30:34 +0000

    update

--- emi/test.sh
+++ emi/test.sh
@@ -1,6 +1,6 @@
-python check_sanity.py fem/prime_single.py
+python check_sanity.py fem/prime_single.py -ncases 1 -param_kappa 1 -param_eps 1E-2
 
-FILE=/home/fenics/emi-book-fem/emi/results/sanity_fem.prime_single_standard_kappa1_eps1.txt
+FILE=/home/fenics/emi-book-fem/emi/results/sanity_fem.prime_single_standard_kappa1_eps0.01.txt
 if test -f "$FILE"; then
     echo "You are all set"
 fi
